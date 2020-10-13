00db18b3660a3501118cd0a7f1c7f5c11f129fab
LinQing <m15651620957@163.com>
2020-10-13 18:53:30 +0800

    修改shell为批量执行工具

--- src/tools/shell.go
+++ src/tools/shell.go
@@ -1,37 +1,34 @@
 package tools
 
 import (
+	"flag"
 	"fmt"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"kube-tools/src/config"
 	"os"
 	"strings"
 )
 
 func Shell() {
-	pods, err := config.KubeClientSet.CoreV1().Pods(config.ShellNamespace).List(metav1.ListOptions{
-		TypeMeta:      metav1.TypeMeta{},
-		LabelSelector: "name=" + config.ShellPodName,
-	})
-
-	if err != nil {
-		fmt.Println("请先安装shell工具")
-		panic(err.Error())
-	}
-
-	for i := 0; i < len(pods.Items); i++ {
-		pod := pods.Items[i]
-		if strings.EqualFold(pod.Status.HostIP, *config.NodeIP) {
-			shellExecOps := ExecOptions{
-				Command:       "bash",
-				ContainerName: "",
-				In:            os.Stdin,
-				Out:           os.Stdout,
-				Err:           os.Stderr,
-				Istty:         true,
+	pods, _ := GetShellPodList()
+	cmdStr := strings.Join(flag.Args()[1:], " ")
+	if pods != nil {
+		for i := 0; i < len(pods.Items); i++ {
+			pod := pods.Items[i]
+			tChan := make(chan int)
+			if strings.Contains(pod.Name, config.ShellPodName) &&
+				(strings.EqualFold(*config.NodeIP, "") || strings.EqualFold(*config.NodeIP, pod.Status.HostIP)) {
+				fmt.Println("------------------------------> No.", i, " Shell on node: "+pod.Status.HostIP+" <------------------------------")
+				dfExecOps := ExecOptions{
+					Command:       cmdStr,
+					ContainerName: "",
+					In:            nil,
+					Out:           os.Stdout,
+					Err:           os.Stderr,
+					Istty:         false,
+				}
+				go ExecCmdParallel(&pod, dfExecOps, tChan)
+				WaitAllThreadFinish(1, tChan, 10)
 			}
-			ExecCmd(&pod, shellExecOps)
-			return
 		}
 	}
 }
