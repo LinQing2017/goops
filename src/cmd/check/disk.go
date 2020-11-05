fbe1422ddb3eb32d0d42fe73e268eff0cf90810f
LinQing2017 <m15651620957@163.com>
2020-11-05 22:55:31 +0800

    添加磁盘检查和清理功能

--- src/cmd/check/disk.go
+++ src/cmd/check/disk.go
@@ -2,15 +2,28 @@ package check
 
 import (
 	"fmt"
+	"github.com/modood/table"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"kube-tools/src/util"
+	"os"
+	"path"
 	"strconv"
 	"strings"
 )
 
-func addDiskFlag(flags *pflag.FlagSet) {
+var (
+	nodename       string
+	dockerRootPath string
+	logRootPath    string
+	cleanLog       bool
+)
 
+func addDiskFlag(flags *pflag.FlagSet) {
+	flags.StringVar(&nodename, "nodename", "", "当前节点Node名称。")
+	flags.StringVar(&dockerRootPath, "docker-root-path", "/data/var/lib/docker", "docker服务数据目录。")
+	flags.StringVar(&logRootPath, "log-root-path", "/data/container_logs", "容器日志根目录。")
+	flags.BoolVar(&cleanLog, "cleanLog", false, "是否清理容器日志。")
 }
 
 func NewCmdDisk() *cobra.Command {
@@ -26,27 +39,52 @@ func NewCmdDisk() *cobra.Command {
 	return cmd
 }
 
+type DiskUsageInfo struct {
+	PodName       string
+	ContainerSize string
+	LogSize       string
+}
+
 func RunDisk(cmd *cobra.Command, args []string) {
-	//cli := util.DockerClient("tcp://172.24.135.47:4243")
-	////containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	//if err != nil {
-	//	panic(err)
-	//}
 
 	kubeClientSet, _ := util.KubeClient(cmd)
-
 	allPods, _ := util.GetPodDict(kubeClientSet, "")
 
-	pods := allPods["172.24.135.10"]
+	if strings.EqualFold(nodename, "") {
+		nodename, _ = os.Hostname()
+	}
+	pods := allPods[nodename]
+	if pods == nil || len(pods) == 0 {
+		fmt.Printf("%s 不是Kubernetes集群的节点或者该节点没有Pod运行")
+		return
+	}
 
+	diskUsageInfos := make([]DiskUsageInfo, 0)
+	// 统计运行Pod磁盘使用情况
+	dockerContainerBasePath := path.Join(dockerRootPath, "containers/")
 	for _, pod := range pods {
 
+		var containerSize int64
+
 		for _, container := range pod.Status.ContainerStatuses {
-
-			containerDataPath := strings.Replace(container.ContainerID, "docker://", "/data/var/lib/docker/containers/", -1)
+			containerDataPath := strings.Replace(container.ContainerID, "docker://", dockerContainerBasePath, -1)
 			size, _ := util.CalculateDirSize(containerDataPath)
-			fmt.Printf("%s : %s mb\n", containerDataPath, strconv.Itoa(int(size)))
+			containerSize += size
 		}
+		logDirPath := path.Join(logRootPath, pod.Name)
 
+		_, isExist := os.Stat(logDirPath)
+		var logDirSize int64
+		if isExist == nil {
+			logDirSize, _ = util.CalculateDirSize(logDirPath)
+		}
+		info := DiskUsageInfo{
+			PodName:       pod.Name,
+			ContainerSize: strconv.FormatFloat(float64(containerSize)/1024/1024, 'f', 2, 64) + " Mi",
+			LogSize:       strconv.FormatFloat(float64(logDirSize)/1024/1024, 'f', 2, 64) + " Mi",
+		}
+		diskUsageInfos = append(diskUsageInfos, info)
 	}
+	nodeInfoStr := table.Table(diskUsageInfos)
+	fmt.Println(nodeInfoStr)
 }
