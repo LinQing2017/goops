eee96af2e37dd840ba106185d12e2e264f520b49
LinQing <m15651620957@163.com>
2020-11-13 16:21:25 +0800

    fix check disk

--- src/cmd/check/appinfo.go
+++ src/cmd/check/appinfo.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
-	"github.com/modood/table"
+	mapset "github.com/deckarep/golang-set"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"go.mongodb.org/mongo-driver/bson"
@@ -26,6 +26,7 @@ var (
 	mongoPasswd string
 	mongoUrl    string
 	mongoDB     string
+	nodeFilter  string
 )
 
 func addAppInfoFlag(flags *pflag.FlagSet) {
@@ -33,6 +34,7 @@ func addAppInfoFlag(flags *pflag.FlagSet) {
 	flags.StringVar(&mongoPasswd, "mongo-password", "lcsX5zq9QEUW", "SDP-K8S Mongodb 密码。")
 	flags.StringVar(&mongoDB, "mongo-database", "qa_mdb_k8s_paas_api", "SDP-K8S Mongodb 数据库名称。")
 	flags.StringVar(&mongoUrl, "mongo-url", "m3.all.debug2.mongod3.sdp:34003,m1.all.debug2.mongod3.sdp:34003,m2.all.debug2.mongod3.sdp:34003", "SDP-K8S Mongodb 地址。")
+	flags.StringVar(&nodeFilter, "node-filter", "172.24.135.12", "根据node名称过滤信息，使用逗号分隔")
 }
 
 func NewCmdAppInfo() *cobra.Command {
@@ -83,12 +85,13 @@ func RunAppInfo(cmd *cobra.Command, args []string) {
 			}
 			appinfo.PodNode = append(appinfo.PodNode, pod.Status.HostIP)
 		}
+		appinfo.PodNode = util.RemoveRepeatedElement(appinfo.PodNode)
 		appinfoList = append(appinfoList, appinfo)
 
 	}
-	infoStr := table.Table(appinfoList)
-	fmt.Println(infoStr)
-	excelOutput(appinfoList)
+
+	appinfoFilter := filter(appinfoList)
+	excelOutput(appinfoFilter)
 }
 
 func getClusterInfo() map[string][]entity.ClusterInfo {
@@ -119,6 +122,27 @@ func getClusterInfo() map[string][]entity.ClusterInfo {
 	return dict
 }
 
+func filter(appInfoList []AppInfo) []AppInfo {
+	if strings.EqualFold(nodeFilter, "") {
+		return appInfoList
+	}
+	nodeFilterSet := mapset.NewSet()
+	for _, v := range strings.Split(nodeFilter, ",") {
+		nodeFilterSet.Add(v)
+	}
+
+	appInfoListFilter := make([]AppInfo, 0)
+	for _, appInfo := range appInfoList {
+		for _, node := range appInfo.PodNode {
+			if nodeFilterSet.Contains(node) {
+				appInfoListFilter = append(appInfoListFilter, appInfo)
+				break
+			}
+		}
+	}
+	return appInfoListFilter
+}
+
 func excelOutput(appInfoList []AppInfo) {
 
 	title := map[string]string{"A1": "序号", "B1": "应用名称", "C1": "命名空间", "D1": "Pod运行节点"}
@@ -134,7 +158,7 @@ func excelOutput(appInfoList []AppInfo) {
 			"A" + rowNum: strconv.Itoa(id),
 			"B" + rowNum: appinfo.Name,
 			"C" + rowNum: appinfo.APPID,
-			"D" + rowNum: strings.Join(util.RemoveRepeatedElement(appinfo.PodNode), ","),
+			"D" + rowNum: strings.Join(appinfo.PodNode, ","),
 		}
 		for k, v := range row {
 			f.SetCellValue("Sheet1", k, v)
