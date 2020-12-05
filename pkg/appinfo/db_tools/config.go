25900b26673ba2f8c5a2130d9bc534caaf98887a
LinQing <m15651620957@163.com>
2020-12-05 13:33:20 +0800

    feature：重构一下

--- pkg/appinfo/db_tools/config.go
+++ pkg/appinfo/db_tools/config.go
@@ -15,6 +15,6 @@ var (
 func AddDBFlag(flags *pflag.FlagSet) {
 	flags.StringVar(&PortalMongoUser, "mongo-user", os.Getenv("PORTAL_MONGODB_USER"), "Mongodb 用户名。")
 	flags.StringVar(&PortalMongoPasswd, "mongo-password", os.Getenv("PORTAL_MONGODB_PASSWORD"), "Mongodb 密码。")
-	flags.StringVar(&PortalMongoUrl, "mongo-database", os.Getenv("PORTAL_MONGODB_DATABASE"), "Mongodb 数据库名称。")
-	flags.StringVar(&PortalMongoDB, "mongo-url", os.Getenv("PORTAL_MONGODB_URL"), "Mongodb 地址。")
+	flags.StringVar(&PortalMongoDB, "mongo-database", os.Getenv("PORTAL_MONGODB_DATABASE"), "Mongodb 数据库名称。")
+	flags.StringVar(&PortalMongoUrl, "mongo-url", os.Getenv("PORTAL_MONGODB_URL"), "Mongodb 地址。")
 }
