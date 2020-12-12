490c760d4aeaec08b718ff14f30a805d9baf0e5a
LinQing2017 <m15651620957@163.com>
2020-12-12 17:51:42 +0800

    fix:删除yaml文件配置

--- chart/sdp-centos/build.sh
+++ chart/sdp-centos/build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
 
-rm -rf /var/www/html/cicd/sdp-app-cli
-cd ../../
-go build -o bin/sdp-app-cli cmd/appinfo/sdp_appinfo.go
-cp bin/sdp-app-cli /var/www/html/cicd/
-cd -
-docker rmi wxext-registry.101.com/sdp/centos:centos7-v1.1
-docker build -t wxext-registry.101.com/sdp/centos:centos7-v1.1 ./
-docker push wxext-registry.101.com/sdp/centos:centos7-v1.1
+
+docker rmi wxext-registry.101.com/sdp/centos:centos7-v1.2
+docker build -t wxext-registry.101.com/sdp/centos:centos7-v1.2 ./
+docker push wxext-registry.101.com/sdp/centos:centos7-v1.2
