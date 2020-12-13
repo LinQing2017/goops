da50e8bad4520c1607c90c8d5615dd76cae0801b
LinQing2017 <m15651620957@163.com>
2020-12-13 11:56:46 +0800

    fix：json字符串上色

--- pkg/util/sys/json.go
+++ pkg/util/sys/json.go
@@ -1,20 +1,15 @@
 package sys
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
+	"github.com/nwidger/jsoncolor"
 	"github.com/sirupsen/logrus"
 )
 
 func PrintJSON(appInfo interface{}) {
-	if jsonByte, err := json.Marshal(appInfo); err == nil {
-		var jsonFormate bytes.Buffer
-		if err = json.Indent(&jsonFormate, jsonByte, "", "    "); err != nil {
-			logrus.Error("解析json字符传异常")
-		} else {
-			fmt.Println(jsonFormate.String())
-		}
+
+	if jsonByte, err := jsoncolor.MarshalIndent(appInfo, "", "    "); err == nil {
+		fmt.Println(string(jsonByte))
 	} else {
 		logrus.Error("解析json字符传异常")
 	}
