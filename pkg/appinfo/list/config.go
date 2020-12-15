db5059edee996bc8637b8b8fe4fa5c514253328c
LinQing <m15651620957@163.com>
2020-12-15 20:46:57 +0800

    fix: 优化参数

--- pkg/appinfo/list/config.go
+++ pkg/appinfo/list/config.go
@@ -13,9 +13,9 @@ var (
 )
 
 func AddListFlag(flags *pflag.FlagSet) {
-	flags.StringVar(&nameFromFile, "name-from-file", "apps-list", "从文件中获取APP信息。")
+	flags.StringVarP(&nameFromFile, "name-from-file", "f", "apps-list", "从文件中获取APP信息。")
 	flags.IntVar(&envType, "env", 0, "环境ID。")
-	flags.StringVarP(&outputFormate, "output-formate", "o", "default", "输出类型。")
+	flags.StringVarP(&outputFormate, "output-formate", "o", "all", "输出类型。")
 	flags.StringVar(&kubeConfig, "kubeconfig", "", "源Kubernete集群的config配置文件。")
 	flags.BoolVar(&withUrl, "url", false, "打印连接地址。")
 }
